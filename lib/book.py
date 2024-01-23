30e44eda02d4c246acdc4fc2f7a4aaec3b560de4
Leon <leon.muriuki@student.moringaschool.com>
2024-01-23 22:10:49 +0300

    Completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 
+# book.py
+
+# book.py
+
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        try:
+            int(value)  # Attempt to convert to an integer
+        except ValueError:
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
-#!/usr/bin/env python3
+# shoe.py
 
 class Shoe:
-    pass
+    def __init__(self, brand=None, size=None):
+        self._brand = brand
+        self._size = size
+        self._condition = None  # Initialize _condition attribute
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        try:
+            int(value)  # Attempt to convert to an integer
+        except ValueError:
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")  # Update the output message
+        self._condition = "New"  # Set _condition to "New" after repair
+
+    @property
+    def condition(self):
+        return self._condition
+
